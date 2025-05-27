10c63296d2f1b3b7078b632feadccd9d68c5ae37
Sakuni1004 <sakunikakarunathilaka99@gmail.com>
2025-05-27 08:13:38 +0530

    add

--- backend/src/routes/authRoute.ts
+++ backend/src/routes/authRoute.ts
@@ -1,11 +1,12 @@
 import express from 'express';
-import {signup, login, refreshAccessToken} from '../controllers/authController';
+import {signup, login, refreshAccessToken, updateUserByIdController} from '../controllers/authController';
 
 const authRouter = express.Router();
 
 authRouter.post('/signup', signup);
 authRouter.post('/login', login);
 authRouter.post('/refresh-token', refreshAccessToken);
+authRouter.put('/update/:id',updateUserByIdController);
 
 
 export default authRouter;
